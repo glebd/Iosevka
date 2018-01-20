a591b3a4c67a6c6a3c913027242cc7ec6a615322
Gleb Dolgich <glebd70@gmail.com>
2018-01-20 20:01:50 +0000

    Use double-storey g

--- make_glebd.sh
+++ make_glebd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
-make custom-config design='expanded v-asterisk-low v-dollar-open v-numbersign-slanted v-g-singlestorey v-at-fourfold cv46'
+make custom-config design='expanded v-asterisk-low v-dollar-open v-numbersign-slanted v-at-fourfold cv46'
 make custom
