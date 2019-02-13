404bf02973f5c2ee75a7e6ddba8f9987f93063ed
Gleb Dolgich <glebd70@gmail.com>
2019-02-13 11:53:00 +0000

    Make custom font set ‘gbd’

--- make_glebd.sh
+++ make_glebd.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -x
-make custom-config design='expanded v-asterisk-low v-dollar-open v-numbersign-slanted v-at-fourfold cv46'
-make custom
+make custom-config set='gbd' design='v-asterisk-low v-dollar-open v-numbersign-slanted v-at-fourfold cv46'
+make custom set='gbd'
+make custom-config set='gbdexp' design='expanded v-asterisk-low v-dollar-open v-numbersign-slanted v-at-fourfold cv46'
+make custom set='gbdexp'
